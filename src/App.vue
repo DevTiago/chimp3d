0000b471fe68b7dbab87b4f865c309f4b7653b54
DevTiago <41440390+DevTiago@users.noreply.github.com>
2021-11-05 10:18:43 +0000

    new layout fix

--- src/App.vue
+++ src/App.vue
@@ -1,42 +1,86 @@
 <template>
-  <div id="app">
-    <Navbar />
-    <Hero />
-    <Services />
-    <Projects />
-  </div>
+  <v-app id="inspire">
+    <v-main>
+      <router-view></router-view>
+    </v-main>
+    <v-fab-transition>
+      <v-btn
+        v-scroll="onScroll"
+        v-show="fab"
+        fab
+        color="white"
+        fixed
+        width="50"
+        height="50"
+        bottom
+        right
+        @click="toTop"
+        class="scroll-fab"
+      >
+        <svg
+          stroke="currentColor"
+          fill="none"
+          stroke-width="2"
+          viewBox="0 0 24 24"
+          stroke-linecap="round"
+          stroke-linejoin="round"
+          height="1em"
+          width="1em"
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <polyline points="18 15 12 9 6 15"></polyline>
+        </svg>
+      </v-btn>
+    </v-fab-transition>
+  </v-app>
 </template>
 
 <script>
-// @ is an alias to /src
-import Navbar from "./components/Navbar.vue";
-import Hero from "./components/Hero.vue";
-import Services from "./components/Services.vue";
-import Projects from "./components/Projects.vue";
-
-export default {
-  name: "App",
-  components: {
-    Navbar,
-    Hero,
-    Services,
-    Projects,
-  },
-};
+  export default {
+    data: () => ({
+      fab: false,
+    }),
+    methods: {
+      onScroll(e) {
+        if (typeof window === "undefined") return;
+        const top = window.pageYOffset || e.target.scrollTop || 0;
+        this.fab = top > 60;
+      },
+      toTop() {
+        this.$vuetify.goTo(0);
+      },
+    },
+  };
 </script>
-pt>
 
-<style lang="scss">
-@font-face {
-  font-family: "Josefin";
-  src: local("Josefin"), url(./fonts/Josefin/JosefinSans-VariableFont_wght.ttf);
-}
+<style scoped lang="scss">
+  .v-btn--fab.v-btn--contained {
+    box-shadow: 0 0 10px 0 rgba(0, 0, 0, 0.1);
+  }
+  .v-btn--icon.v-size--default .v-icon,
+  .v-btn--fab.v-size--default .v-icon {
+    height: 30px;
+    font-size: 30px;
+    width: 30px;
+  }
+  .v-btn--fab.v-btn--fixed {
+    z-index: 99;
+  }
+  .v-btn--absolute.v-btn--bottom,
+  .v-btn--fixed.v-btn--bottom {
+    bottom: 60px;
+  }
 
-#app {
-  font-family: "Josefin", Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
+  .v-btn--absolute.v-btn--right,
+  .v-btn--fixed.v-btn--right {
+    right: 20px;
+  }
+  .scroll-fab {
+    &.white {
+      border: none;
+    }
+    svg {
+      font-size: 24px;
+    }
+  }
 </style>
--- src/main.js
+++ src/main.js
@@ -1,21 +1,24 @@
-import Vue from "vue";
-import App from "./App.vue";
-import router from "./router";
-import store from "./store";
-import { BootstrapVue, IconsPlugin } from "bootstrap-vue";
-
-import "bootstrap/dist/css/bootstrap.css";
-import "bootstrap-vue/dist/bootstrap-vue.css";
-
-// Make BootstrapVue available throughout your project
-Vue.use(BootstrapVue);
-// Optionally install the BootstrapVue icon components plugin
-Vue.use(IconsPlugin);
+import Vue from 'vue'
+import App from './App.vue'
+import router from './router'
+import vuetify from './plugins/vuetify';
+import VueParticles from "vue-particles";
+import CoolLightBox from 'vue-cool-lightbox';
+import VueScrollactive from 'vue-scrollactive';
+import "vue-slick-carousel/dist/vue-slick-carousel.css";
+import "vue-slick-carousel/dist/vue-slick-carousel-theme.css";
+import '@fortawesome/fontawesome-free/css/all.css'
+import 'vue-cool-lightbox/dist/vue-cool-lightbox.min.css'
+import './assets/scss/main.scss'
 
 Vue.config.productionTip = false;
+Vue.use(VueParticles);
+Vue.use(CoolLightBox);
+Vue.use(VueScrollactive);
+
 
 new Vue({
-  router,
-  store,
-  render: (h) => h(App),
-}).$mount("#app");
+    router,
+    vuetify,
+    render: h => h(App)
+}).$mount('#app')
