2fa13f63746cdb92bb35e2e5517d65080869c8fc
DevTiago <41440390+DevTiago@users.noreply.github.com>
2022-02-22 09:04:58 +0000

    index images fix

--- src/components/index/PortfolioTwo.vue
+++ src/components/index/PortfolioTwo.vue
@@ -50,13 +50,13 @@ export default {
 };
 </script>
 
-<style>
+<style >
 .im_portfolio {
   display: flex;
   align-items: center;
   justify-content: center;
   padding: 10px;
-  height: 280px
+  height: 280px!important;
 }
 
 .thumbnail {
